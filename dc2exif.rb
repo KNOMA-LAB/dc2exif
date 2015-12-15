c2e113156f3c66ae8df9c07945136c6bbfdb12e5
Carlos Agarie <carlos.agarie@gmail.com>
2015-12-15 13:24:26 -0200

    It works :)

--- dc2exif.rb
+++ dc2exif.rb
@@ -9,15 +9,32 @@ require 'json'
 # Para cada chave:valor na config,
 #   Pega a chave no DC e escreve no `valor` do EXIF.
 
+def config_and_attr_equal?(config, value, attr)
+  value.attr(attr) == config[attr]
+end
+
+def find_value_for_config(values, config)
+  value = values.find do |v|
+    [
+      config_and_attr_equal?(config, v, "element"),
+      config_and_attr_equal?(config, v, "qualifier"),
+      config_and_attr_equal?(config, v, "language"),
+    ].all?
+  end
+
+  [config["to"], value.text]
+end
+
 CONFIG = JSON.load(File.open('config.json'))
 image = MiniExiftool.new("resolver.jpeg")
 dublin_core = Nokogiri::XML(File.open("dublin_core.xml"))
 
 dcvalues = dublin_core.xpath("//dublin_core//dcvalue")
-dcvalues.each do |v|
-  puts v.attr("element"), v.text
+
+CONFIG.each do |c|
+  v = find_value_for_config(dcvalues, c)
+  image[v[0]] = v[1]
+  puts image[v[0]]
 end
 
-title_node = dcvalues.find { |v| v.attr("element") == "title" }
-image.title = title_node.text
 image.save
